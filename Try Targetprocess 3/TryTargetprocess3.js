297ab6c2ec25809705a59b5117ef9ff3338341b0
Dmitry Kanashevich <kanashevich@targetprocess.com>
2013-07-31 15:32:40 +0300

    Added Try Targetprocess 3 mashup

---
+++ Try Targetprocess 3/TryTargetprocess3.js
@@ -0,0 +1,5 @@
+tau.mashups
+    .addDependency('jQuery')
+    .addMashup(function ($) {
+        $('ul.user-sub').append('<li style="padding-top: 0px"><a href="http://localhost/restui/board.aspx" style="border: 1px solid #7daa43; background: #7daa43; background: -webkit-gradient(linear, left top, left bottom, from(#a0c74b), to(#7daa43)); background: -webkit-linear-gradient(top, #a0c74b, #7daa43); background: -moz-linear-gradient(top, #a0c74b, #7daa43); background: -ms-linear-gradient(top, #a0c74b, #7daa43); background: -o-linear-gradient(top, #a0c74b, #7daa43); background-image: -ms-linear-gradient(top, #a0c74b 0%, #7daa43 100%); padding: 4px 8px; -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px; -webkit-box-shadow: rgba(255,255,255,0.4) 0 0px 0, inset rgba(255,255,255,0.4) 0 1px 0; -moz-box-shadow: rgba(255,255,255,0.4) 0 0px 0, inset rgba(255,255,255,0.4) 0 1px 0; box-shadow: rgba(255,255,255,0.4) 0 0px 0, inset rgba(255,255,255,0.4) 0 1px 0; text-shadow: #649b30 0 1px 0; color: #ffffff; font-size: 12px; font-family: helvetica, serif; text-decoration: none; vertical-align: middle;">Try Targetprocess<sup>3</sup></a></li>');
+    });
