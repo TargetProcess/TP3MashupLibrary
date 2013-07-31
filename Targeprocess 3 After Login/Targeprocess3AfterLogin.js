12e47ef8ea905d11cc5ed98c5c1afb21531f2eb0
Dmitry Kanashevich <kanashevich@targetprocess.com>
2013-07-31 14:35:18 +0300

    Fixed url in Targetprocess 3 After Login mashup

--- Targeprocess 3 After Login/Targeprocess3AfterLogin.js
+++ Targeprocess 3 After Login/Targeprocess3AfterLogin.js
@@ -2,6 +2,6 @@ tau.mashups
     .addMashup(function () {
         if (document.referrer.toLowerCase().indexOf('login.aspx') != -1
             && window.location.href.toLowerCase().indexOf('restui/board.aspx') == -1){
-            window.location.href = 'http://localhost/targetprocess/restui/board.aspx';
+            window.location.href = 'http://localhost/restui/board.aspx';
         }
     });
