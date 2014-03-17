b70a03606038836f630a80de48cbc4a3a23fcf81
Aliaksei Shytkin <e79eas@gmail.com>
2014-03-17 11:57:51 +0300

    US77540 Improve embedded pages to work faster

--- Embedded Pages/EmbeddedPages.config.js
+++ Embedded Pages/EmbeddedPages.config.js
@@ -1,16 +1,18 @@
 tau.mashups
     .addModule('EmbeddedPages.config', function() {
+        'use strict';
         var EmbeddedPagesConfig = {
             tabs: [{
                 /*Sample embedded page of the 'CustomPageUrl' Custom Field of a User Story of a Project with the 'Scrum' Process*/
                 entityTypeName: 'UserStory',
-                customFieldName: 'CustomPageUrl',
-                processName: 'Scrum'
-            },{
+                customFieldName: 'Image of the day',
+                processName: 'Scrum',
+                frameTemplate: '<iframe src="${url}" width="854" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+            }, {
                 /*Sample embedded page of the 'CustomPageUrl' Custom Field of a Bug of any Project*/
                 entityTypeName: 'Bug',
-                customFieldName: 'CustomPageUrl'				
-            },{
+                customFieldName: 'CustomPageUrl'
+            }, {
                 /*Sample embedded page of the 'CustomEmbeddedVideoUrl' Custom Field with the overridden template of the tab frame*/
                 entityTypeName: 'Project',
                 customFieldName: 'CustomEmbeddedVideoUrl',
