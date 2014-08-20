072f98597f21607309a1b9e48efa45366b3d9c8b
Aliaksei Shytkin <e79eas@gmail.com>
2014-08-20 14:39:55 +0300

    Bug#86162 Ids for time sheet

--- IDs on Time Sheet/IDsonTimeSheet.js
+++ IDs on Time Sheet/IDsonTimeSheet.js
@@ -1,19 +1,21 @@
-tau.mashups.addDependency('libs/jquery/jquery').addMashup(
-function (config) {
-    function extraTimeSheetInfo() {}
-        extraTimeSheetInfo.prototype = {
-            render: function () {
-                $('div#ctl00_mainArea_pnlUpd > table:first').find('tr.dataRow').each(function() {
-                    /* gather IDs */
-                    var id = $(this).find('td:eq(2) > a:first').attr('href').match(/View\.aspx\?[A-Za-z]+ID=(\d+)/)[1];
-                    $(this).find('td:eq(2)').prepend('<div style="font-size: smaller; float: left; margin-right: 6px;">#'+id+'</div>');
-                });
-            },
-        }
-        /* make it happen! */
-        $(document).ready(function() {
-            new extraTimeSheetInfo().render();
-        });
-    }
-)
+/*globals tau*/
+tau.mashups
+    .addDependency('jQuery')
+    .addMashup(function($) {
 
+        'use strict';
+
+        $(document).ready(function() {
+
+            $('div#ctl00_mainArea_pnlUpd > table:first').find('tr.dataRow').each(function() {
+                var id = $(this).find('td:eq(2) > a:first').attr('href').match(/#(\w+)\/(\d+)/);
+                if (id) {
+                    id = id[2];
+                    $(this).find('td:eq(2)').prepend(
+                        '<div style="font-size: smaller; float: left; margin-right: 6px;">#' +
+                            id +
+                        '</div>');
+                }
+            });
+        });
+    });
