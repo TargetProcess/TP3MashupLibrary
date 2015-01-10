16b0293b06356f01bc6a0d4b2d49cda160dd1054
Yuri Popovski <papouski@targetprocess.com>
2015-01-10 12:42:49 +0300

    US#TP2Pages Corrections
    Create a separate menu section and add a tooltip

--- Targetprocess 2 Pages/Tp2Pages.js
+++ Targetprocess 2 Pages/Tp2Pages.js
@@ -1,10 +1,13 @@
 tau.mashups
     .addDependency('Underscore')
     .addDependency('tp3/mashups/topmenu')
+    .addDependency('tp3/mashups/tooltip')
+    .addDependency('tp3/mashups/viewcomponent')
     .addDependency('tp3/mashups/componenteventlistener')
     .addDependency('Tp2Pages.config')
+    .addDependency('tau/ui/templates/board.main.menu/ui.template.board.main.menu.section')
     .addCSS('Tp2Pages.css')
-    .addMashup(function(_, topmenu, ComponentEventListener, settings) {
+    .addMashup(function(_, topmenu, Tooltip, ViewComponent, ComponentEventListener, settings, menuSectionTemplate) {
         new ComponentEventListener('board.main.menu').on('afterRenderAll', function(event, afterRenderData) {
             // Get the mash-up configuration
             var uris = settings.uris;
@@ -19,9 +22,21 @@ tau.mashups
             }
 
             // Create and fill up the new menu
+            topmenu._$topMenuContainer.prepend(menuSectionTemplate.get().bind());
             var menuItem = topmenu.addItem('TP2');
             _.each(_.keys(uris), function (element) {
                 menuItem.addItem(element).onClick(createMenuItemCallback(uris[element]));
+            });
+
+            // Create tooltip
+            var $tooltipTarget = menuItem.$element.children('.tau-popup-link');
+            new Tooltip(new ViewComponent($tooltipTarget, new ComponentEventListener('TP2 Menu')), {
+                $target: $tooltipTarget,
+                getContentPromise: function() {
+                    var contentDeferred = $.Deferred();
+                    contentDeferred.resolve($('<div><span>Go back to the TP2 interface</span></div>'));
+                    return contentDeferred.promise();
+                }
             });
         });
     });
