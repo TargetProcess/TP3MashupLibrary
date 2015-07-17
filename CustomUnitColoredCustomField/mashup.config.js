4887402247db632158a0e33ad0eb6d2ace57fa8c
Aliaksei Shytkin <e79eas@gmail.com>
2015-07-17 14:19:52 +0300

    Add CustomUnitColoredCustomField mashup

---
+++ CustomUnitColoredCustomField/mashup.config.js
@@ -0,0 +1,18 @@
+tau.mashups.addModule("CustomUnitColoredCustomField/config", [
+    {
+        "name": "Favorite Fruit",
+        "colors": {
+            "Apple": "red",
+            "Pear": "#007f00",
+            "Plum": "rgb(128, 0, 128)"
+        }
+    },
+    {
+        "name": "Your Corps",
+        "colors": {
+            "Green Lanterns": "green",
+            "Red Lanterns": "#7f0000",
+            "Sinestro Corps": "rgb(128, 128, 0)"
+        }
+    }
+]);
