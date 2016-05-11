3024cdfad31fc367dc62f6b67c6a39bf60e9075b
Aliaksei Shytkin <e79eas@gmail.com>
2016-05-11 14:41:35 +0300

    Custom Unit Colored Custom Field v1.0.0

--- Custom Unit Colored Custom Field/mashup.config.js
+++ Custom Unit Colored Custom Field/mashup.config.js
@@ -1,19 +1,21 @@
-tau.mashups.addModule("CustomUnitColoredCustomField/config", [
-    {
-        "name": "Favorite Fruit",
-        "colors": {
-            "Apple": "red",
-            "Pear": "#007f00",
-            "Plum": "rgb(128, 0, 128)",
-            "Tangerine": "orange"
-        }
-    },
-    {
-        "name": "Your Corps",
-        "colors": {
-            "Green Lanterns": "green",
-            "Red Lanterns": "#7f0000",
-            "Sinestro Corps": "rgb(128, 128, 0)"
-        }
-    }
-]);
+tau.mashups.addModule("CustomUnitColoredCustomField/config", 
+[
+    // {
+    //     "name": "Favorite Fruit",
+    //     "colors": {
+    //         "Apple": "red",
+    //         "Pear": "#007f00",
+    //         "Plum": "rgb(128, 0, 128)",
+    //         "Tangerine": "orange"
+    //     }
+    // },
+    // {
+    //     "name": "Your Corps",
+    //     "colors": {
+    //         "Green Lanterns": "green",
+    //         "Red Lanterns": "#7f0000",
+    //         "Sinestro Corps": "rgb(128, 128, 0)"
+    //     }
+    // }
+]
+);
