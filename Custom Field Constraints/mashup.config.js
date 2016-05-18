61c521bafbb6c096edb78c379673bc80c9f52bb8
Aliaksei Shytkin <e79eas@gmail.com>
2016-05-18 12:23:43 +0300

    Custom Field Constraints v1.2.0

--- Custom Field Constraints/mashup.config.js
+++ Custom Field Constraints/mashup.config.js
@@ -2,65 +2,7 @@ tau.mashups.addModule("CustomFieldConstraints/config", [
     {
         "process": "Kanban",
         "constraints": {
-            "userstory": {
-                "entityStates": [
-                    {
-                        "name": "Open",
-                        "requiredCustomFields": [
-                            "Cf1",
-                            "Cf3"
-                        ]
-                    }
-                ],
-                "customFields": [
-                    {
-                        "name": "Cf1",
-                        "valueIn": [
-                            "Cf1ValueThatRequiresCf2"
-                        ],
-                        "requiredCustomFields": [
-                            "Cf2"
-                        ]
-                    },
-                    {
-                        "name": "Cf3",
-                        "valueIn": [
-                            "Cf3ValueThatRequiresCf4"
-                        ],
-                        "requiredCustomFields": [
-                            "Cf4"
-                        ]
-                    }
-                ]
-            },
-            "task": {
-                "entityStates": [
-                    {
-                        "name": "Done",
-                        "requiredCustomFields": [
-                            "Cf1"
-                        ]
-                    }
-                ]
-            },
-            "project": {
-                "customFields": [
-                    {
-                        "name": "Cf1",
-                        "valueNotIn": [
-                            "Cf1ValueThatDoesNotRequireCf2"
-                        ],
-                        "requiredCustomFields": [
-                            "Cf2"
-                        ]
-                    }
-                ]
-            }
-        }
-    },
-    {
-        "constraints": {
-            "user": {
+            "userStory": {
                 "customFields": [
                     {
                         "name": "Age",
