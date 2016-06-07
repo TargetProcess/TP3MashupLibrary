64c150b2df6b9274480b3878c7dc4dcb338031d9
Alex Fomin <ShurickFomin@gmail.com>
2016-06-07 11:28:55 +0300

    Fix review issues

--- Custom Unit Project Name On Lists For Relation Cards/ProjectNameOnLists.js
+++ Custom Unit Project Name On Lists For Relation Cards/ProjectNameOnLists.js
@@ -1,38 +1,30 @@
 tau.mashups
-    .addDependency('jQuery')
     .addDependency('Underscore')
     .addDependency('tau/configurator')
-    .addDependency('tau/models/board.customize.units/const.entity.types.names')      
+    .addDependency('tau/models/board.customize.units/const.entity.types.names')
     .addDependency('tau/models/board.customize.units/const.card.sizes')
-    .addMashup(function($, _, globalConfigurator, types, sizes) {
+    .addMashup(function (_, globalConfigurator, types, sizes) {
 
         function addUnits(configurator) {
             var unitRegistry = configurator.getUnitsRegistry();
-            var projectLong = unitRegistry.getUnitById('project_long');    
+            var projectLong = unitRegistry.getUnitById('project_long');
             if (projectLong) {
-              var settingsToExtend = _.find(projectLong.settings, function(setting){
-                return _.contains(setting.types, types.STORY) &&
-                 _.contains(setting.sizes, sizes.LIST) 
-              });     
-              if (settingsToExtend) {
-                settingsToExtend.types.push(types.INBOUND_RELATION_CARD);
-                settingsToExtend.types.push(types.OUTBOUND_RELATION_CARD);
-              }                                                               
+                var settingsToExtend = _.find(projectLong.settings, function (setting) {
+                    return _.contains(setting.types, types.STORY) &&
+                        _.contains(setting.sizes, sizes.LIST)
+                });
+                if (settingsToExtend) {
+                    settingsToExtend.types.push(types.INBOUND_RELATION_CARD);
+                    settingsToExtend.types.push(types.OUTBOUND_RELATION_CARD);
+                }
             }
         }
 
-
         var appConfigurator;
-        globalConfigurator.getGlobalBus().on('configurator.ready', function(e, configurator) {
-
+        globalConfigurator.getGlobalBus().on('configurator.ready', function (e, configurator) {
             if (!appConfigurator && configurator._id && configurator._id.match(/board/)) {
-
                 appConfigurator = configurator;
                 addUnits(appConfigurator);
-
             }
-
         });
-
-
- });
+    });
