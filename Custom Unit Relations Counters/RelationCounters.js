f6575fc84441afdce094bfcdf0289940b389dce5
Alex Fomin <ShurickFomin@gmail.com>
2016-06-09 19:54:48 +0300

    Custom Units - Open/Total Work Items and Relations without Links

--- Custom Unit Relations Counters/RelationCounters.js
+++ Custom Unit Relations Counters/RelationCounters.js
@@ -10,6 +10,9 @@ tau.mashups
                 id: 'custom_open_total_work_items',
                 classId: 'tau-board-unit_type_open_total_work_items',
                 name: 'Open/Total Work Items',
+                hideIf: function(data) {
+                    return !data.total && !data.open;
+                },
                 types: [
                     et.FEATURE, et.EPIC, et.STORY, et.TASK, et.BUG, et.REQUEST, et.IMPEDIMENT,
                     et.RESPONSIBLE_TEAM, et.TEAM, et.PROJECT, et.ITERATION, et.TEAM_ITERATION, et.RELEASE,
@@ -17,17 +20,24 @@ tau.mashups
                 ],
                 sizes: [sz.XS, sz.S, sz.M, sz.L, sz.XL, sz.LIST],
                 template: [
-                    '<div class="tau-board-unit__value-open"><%! this.data.open||"0" %> / <%! this.data.total||"0" %></div>'
+                    '<div class="tau-board-unit__value-open"><%! this.data.open||"0" %>/<%! this.data.total||"0" %></div>'
                 ],
-                model: 'total:OutboundAssignables.Where(RelationType.Name=="Link").count(),' +
-                'open:OutboundAssignables.Where(RelationType.Name=="Link" and EntityState.IsFinal!=true).count()'
-                ,
+                listSettings: {
+                    title: [
+                        'Open: <%! this.data.open||"0" %>, Total: <%! this.data.total||"0" %>'
+                    ]
+                },
+                model:
+                'total:OutboundAssignables.Where(RelationType.Name=="Link").count()+' +
+                'SlaveRelations.Where(RelationType.Name=="Link" and slave.entityType.name="Project").count(),' +
+                'open:OutboundAssignables.Where(RelationType.Name=="Link" and EntityState.IsFinal!=true).count()+' +
+                'SlaveRelations.Where(RelationType.Name=="Link" and slave.as<Project>.EntityState.IsFinal!=true).count()',
                 sampleData: {open: 5, total: 10}
             },
             {
                 id: 'custom_relation_not_links',
                 classId: 'tau-board-unit_type_relations-counter-in-out',
-                name: "Relations without Links",
+                name: "Work Relations",
                 types: [
                     et.FEATURE, et.EPIC, et.STORY, et.TASK, et.BUG, et.REQUEST, et.IMPEDIMENT,
                     et.RESPONSIBLE_TEAM, et.TEAM, et.PROJECT, et.ITERATION, et.TEAM_ITERATION, et.RELEASE,
@@ -45,6 +55,14 @@ tau.mashups
                 sampleData: {
                     inbound: 10,
                     outbound: 20
+                },
+                hideIf: function(data) {
+                    return !data.inbound && !data.outbound;
+                },
+                listSettings: {
+                    title: [
+                        'In: <%! this.data.inbound||"0" %>, out: <%! this.data.outbound||"0" %>'
+                    ]
                 },
                 model: 'inbound:masterRelations.Where(RelationType.Name !="Link").Count,' +
                 'outbound:slaveRelations.Where(RelationType.Name !="Link").Count'
