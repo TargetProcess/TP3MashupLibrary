308f4bd00f9ee6e55e5e9ef664973c88aa57da38
Ilya Tsuryev <i.tsuryev@gmail.com>
2016-09-05 18:06:42 +0300

    Update Share View to v3.0.6

--- Share View/mashup.config.js
+++ Share View/mashup.config.js
@@ -1,4 +1,5 @@
 tau.mashups.addModule("ShareBoard/config", {
     "serviceUrl": "https://tauboard.com",
-    "title": "Share %s"
+    "title": "Share %s",
+    "newActionsListVersion": "3.9.4.0"
 });
