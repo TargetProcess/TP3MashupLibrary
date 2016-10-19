545b24351f1af9e1e04cc3228c9cd136de1cda3b
Victor Homyakov <vkhomyackov@gmail.com>
2016-10-19 20:14:59 +0300

    US#137035 Add option to resize non-default frame; add template variable `entityId`

--- Embedded Pages/mashup.config.js
+++ Embedded Pages/mashup.config.js
@@ -5,6 +5,7 @@ tau.mashups.addModule('EmbeddedPages/config', {
         entityTypeName: 'UserStory',
         customFieldName: 'Image of the day',
         processName: 'Scrum',
+        /* Template could use following variables: url, name, entityId */
         frameTemplate: '<iframe src="${url}" width="854" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
     }, {
         /* Sample embedded page of the 'CustomPageUrl' Custom Field of a User Story of any Project */
@@ -14,11 +15,15 @@ tau.mashups.addModule('EmbeddedPages/config', {
         /* Sample embedded page of the 'CustomEmbeddedVideoUrl' Custom Field with the overridden template of the tab frame */
         entityTypeName: 'Project',
         customFieldName: 'CustomEmbeddedVideoUrl',
-        frameTemplate: '<iframe src="${url}" width="854" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+        /* By default mashup does not adjust frame size when frameTemplate is specified. This behavior could be overridden */
+        adjustFrameSize: true,
+        frameTemplate: function(data) {
+            return '<iframe width="854" height="480" frameborder="0" src="' + data.url + '"' +
+                ' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+        }
     }, {
         /* Embedded page for entity without process */
         entityTypeName: 'User',
         customFieldName: 'Facebook Profile'
     }]
-
 });
