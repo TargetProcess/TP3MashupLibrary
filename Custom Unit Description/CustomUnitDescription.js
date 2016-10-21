5006560bd0e2d4e617f1252300a6abbe6eb693c0
Sergey Truhtanov <truhtanov@targetprocess.com>
2016-10-21 11:37:37 +0300

    Custom Unit Description mashup. Add support for list mode

--- Custom Unit Description/CustomUnitDescription.js
+++ Custom Unit Description/CustomUnitDescription.js
@@ -8,9 +8,9 @@ tau.mashups
 
         var units = [
             {
-             	id: 'custom_rich_text',
+                id: 'custom_rich_text',
                 classId: 'tau-board-unit_type_custom-rich-text',
-	            name: 'Description',
+                name: 'Description',
                 types: [ types.PROJECT, types.FEATURE, types.EPIC, types.STORY, types.TASK, types.BUG, types.REQUEST ],
                 sections: 1,
                 sizes: [ sizes.M, sizes.L, sizes.XL ],
@@ -21,6 +21,27 @@ tau.mashups
                     '<% tmp.innerHTML = this.data.description; %>',
                     '<% var plain = tmp.textContent || tmp.innerText || ""; %>',
                     '<% if(plain.length > 150) { plain = plain.substring(0, 150) + "..."; } %>',
+                    '<%= plain.replace(/\u00a0/g, " ") %>',
+                    '</div>',
+                  '<% } %>'
+                ],
+                model: 'description:Description',
+                sampleData: { description: '<div>Entity description</div>' }
+           },
+           {
+                id: 'custom_rich_text_list',
+                classId: 'tau-board-unit_type_custom-rich-text',
+                name: 'Description',
+                types: [ types.PROJECT, types.FEATURE, types.EPIC, types.STORY, types.TASK, types.BUG, types.REQUEST ],
+                sections: 1,
+                sizes: [ sizes.LIST ],
+                template: [
+                  '<% if(this.data.description) { %>',
+                    '<div class="tau-board-unit__value">',
+                    '<% var tmp = document.createElement("DIV"); %>',
+                    '<% tmp.innerHTML = this.data.description; %>',
+                    '<% var plain = tmp.textContent || tmp.innerText || ""; %>',
+                    '<% if(plain.length > 300) { plain = plain.substring(0, 300); } %>',
                     '<%= plain.replace(/\u00a0/g, " ") %>',
                     '</div>',
                   '<% } %>'
