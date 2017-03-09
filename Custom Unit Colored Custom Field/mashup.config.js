9509ddd9dac5be7c96f9d596596b2243323e388b
Victor Homyakov <vkhomyackov@gmail.com>
2017-03-09 13:30:47 +0300

    US#148623 Add option to configure background color; fix bugs

--- Custom Unit Colored Custom Field/mashup.config.js
+++ Custom Unit Colored Custom Field/mashup.config.js
@@ -1,21 +1,20 @@
-tau.mashups.addModule("CustomUnitColoredCustomField/config", 
-[
-    // {
-    //     "name": "Favorite Fruit",
-    //     "colors": {
-    //         "Apple": "red",
-    //         "Pear": "#007f00",
-    //         "Plum": "rgb(128, 0, 128)",
-    //         "Tangerine": "orange"
-    //     }
-    // },
-    // {
-    //     "name": "Your Corps",
-    //     "colors": {
-    //         "Green Lanterns": "green",
-    //         "Red Lanterns": "#7f0000",
-    //         "Sinestro Corps": "rgb(128, 128, 0)"
-    //     }
-    // }
-]
-);
+tau.mashups.addModule("CustomUnitColoredCustomField/config", [
+    //{
+    //    "name": "Favorite Fruit",
+    //    "colors": {
+    //        "Apple": "red",
+    //        "Pear": "#007f00",
+    //        "Plum": "rgb(128, 0, 128)",
+    //        "Tangerine": "orange",
+    //        "Lemon": {text: "black", background: "rgb(358, 358, 179)"}
+    //    }
+    //},
+    //{
+    //    "name": "Your Corps",
+    //    "colors": {
+    //        "Green Lanterns": "green",
+    //        "Red Lanterns": "#7f0000",
+    //        "Sinestro Corps": "rgb(128, 128, 0)"
+    //    }
+    //}
+]);
